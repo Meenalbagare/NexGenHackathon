b0417bd8b7e0bf6f3aea70964689536982817ba5
Ujwal <ujwalujwalc@gmail.com>
2024-04-06 18:59:14 +0530

    update

--- api.py
+++ api.py
@@ -3,14 +3,20 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import pyotp
+from pydantic import BaseModel
+
+class UserRegistration(BaseModel):
+    email: str
+    name: str
+    password: str
 
 
-async def generateAuthenticationKey():
+def generateAuthenticationKey():
     return pyotp.random_base32()
 
 
 
-async def sendMail(mail, subject, body):
+def sendMail(mail, subject, body):
     try:
         sender_email = "nexgenhackathon@gmail.com"
         receiver_email = mail
@@ -37,11 +43,11 @@ verify_data = {}
 
 
 
-async def otpMail(mail, name, password):
+def otpMail(mail, name, password):
     reqid[0] += 1
     totp = pyotp.TOTP(generateAuthenticationKey(), interval=300)
     body = f"""
-        Dear User,
+        Dear {name},
 
         Thank you for using our service. Here is your One Time Password (OTP):
 
@@ -56,8 +62,8 @@ async def otpMail(mail, name, password):
         """
     ids[reqid[0]] = totp
     if sendMail(mail, "One Time Password (OTP) for NexGen Hackathon", body):
-        verify_data[totp] = [mail, name, password]
-        return {"message": "OTP sent successfully", "request_id": totp}
+        verify_data[reqid[0]] = [mail, name, password]
+        return {"message": "OTP sent successfully", "request_id": reqid[0]}
     else:
         return {"message": "Failed to send OTP", "request_id": None}
 
@@ -67,6 +73,13 @@ app = FastAPI()
 
 
 @app.post("/register/")
-async def register_user(email:str, name:str, password:str):
+def register(user_data: UserRegistration):
+    email = user_data.email
+    name = user_data.name
+    password = user_data.password
+
+    if not (email and name and password):
+        raise HTTPException(status_code=400, detail="Email, name, and password are required.")
+
     return otpMail(email, name, password)
-    
+
