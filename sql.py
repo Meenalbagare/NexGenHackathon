cc717cf70024a4a64ee0d2104664f7f8c01b2255
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-04-06 17:22:11 +0530

    Add Chat History Table Creation

--- sql.py
+++ sql.py
@@ -1,12 +1,14 @@
 import mysql.connector
 
+password = "root"
+
 def create_database():
     try:
         # Establishing connection to MySQL server
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="Password123$"
+            password=password
         )
 
         # Creating a cursor object using the cursor() method
@@ -32,7 +34,7 @@ def create_table():
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="Password123$",
+            password=password,
             database="NexGenDB"
         )
 
@@ -67,6 +69,51 @@ def create_table():
             connection.close()
             print("MySQL connection is closed")
 
-# Call the functions to create the database and the table
-create_database()
-create_table()
+def create_chat_history_table():
+    try:
+        # Establishing connection to MySQL server
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=password,
+            database="NexGenDB"
+        )
+
+        # Creating a cursor object using the cursor() method
+        cursor = connection.cursor()
+
+        # SQL query to create a table
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS chat_history (
+            chat_id INT AUTO_INCREMENT,
+            user_id INT,
+            topic VARCHAR(20),
+            history TEXT,
+            PRIMARY KEY(chat_id, user_id),
+            FOREIGN KEY(user_id) REFERENCES users(id)
+        )
+        """
+
+        drop_table_query = """
+        DROP TABLE IF EXISTS chat_history
+        """
+
+        # Executing the SQL query
+        cursor.execute(drop_table_query)
+        cursor.execute(create_table_query)
+        print("Table created successfully")
+
+    except mysql.connector.Error as error:
+        print("Failed to create table: {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+if __name__ == "__main__":
+    # Call the functions to create the database and the table
+    create_database()
+    create_table()
+    create_chat_history_table()
